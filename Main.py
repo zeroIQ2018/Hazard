f5d61675bc3eb59e439ebcf8c974134ce2eaa649
zeroIQ2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-09-18 22:42:31 +0200

    javascript stuff

---
+++ Main.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("chicken.html")
+
+
+if __name__ == "__main__":
+    app.run()
+
+ 
