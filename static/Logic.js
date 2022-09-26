d5e3617294fc0d1ccf29dbca62e6d782f4acf53e
zeroIQ2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-09-26 17:12:34 +0200

    cum-it

--- static/Logic.js
+++ static/Logic.js
@@ -1,69 +1,74 @@
  
-    //write the choice of the player i forgor how????? why tf is the element with id "3" the one with 
+//write the choice of the player i forgor how????? why tf is the element with id "3" the one with 
 
-    var choice;
-    function clickFunction(choice) 
-    {
-        /*idk why i did this in retarded but im keeping this for the future for flipping a number between 0 and 1
-            if (choice === 0)
-            {
-                choice = 1;
-            }
-        else {
-            choice = 0;
+var choice;
+function clickFunction(choice){
+    /*idk why i did this in retarded but im keeping this for the future for flipping a number between 0 and 1
+        if (choice === 0)
+        {
+            choice = 1;
         }
-        */
-        console.log(choice);
-     }
-
-    // does the choice for the enemy 
-    // its literally just 1 or 0
-
-    
-    var y = Math.random();
-    if (y < 0.5){
-    y = 0
-    }
     else {
-    y = 1
+        choice = 0;
+    }
+    */
+    console.log(choice)
     }
 
-    document.write(y);
-    document.write(choice);
+// does the choice for the enemy 
+// its literally just 1 or 0
 
-    console.log(y);
-    // if statment
-    // or should i use switch statements
-    //does it only check at the start or should i use a loop??????
-while (choice = undefined) 
+var y = Math.random();
+if (y < 0.5){
+y = 0
+}
+else {
+y = 1
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+console.log(y);
+console.log(typeof(y));
+console.log(typeof(choice));
+
+// if statment
+// or should i use switch statements
+//does it only check at the start or should i use a loop??????
+
+if (choice === 1 && y === 1)
 {
-    if (choice === 1 && y === 1)
-    {
-        console.log("11");
-        document.write("Tod");
+    console.log("11");
+    document.log("beide sterben")
+    
+} 
 
-    } 
+else if (choice === 0 && y === 0)
+{
+    console.log("00");
+    document.write("Beide entkommen den Tod");
     
-    else if (choice === 0 && y === 0)
-    {
-        console.log("00");
-        document.write("Beide entkommen den Tod");
-    } 
+} 
+
+else if (choice === 1 && y === 0)
+{
+    console.log("10");
+    document.write("Gegner versucht auszuweichen. Spieler fährt weiter. Spieler gewinnt");
     
-    else if (choice === 1 && y === 0)
-    {
-        console.log("10");
-        document.write("Gegner versucht auszuweichen. Spieler fährt weiter. Spieler gewinnt");
-    } 
+} 
+
+else if (choice === 0 && y === 1) 
+{
+    console.log("01");
+    document.write("Spieler versucht auszuweichen. Gegner fährt weiter. Gegner gewinnt");
     
-    else if (choice === 0 && y === 1) 
-    {
-        console.log("01");
-        document.write("Spieler versucht auszuweichen. Gegner fährt weiter. Gegner gewinnt");
-    } 
+} 
+
+else 
+{
     
-    else 
-    {
-        document.write(" it didnt work");
-    }
+
+
 }
