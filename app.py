2f992dae20f2904542bd0ab93b2cb20222862b01
zero <schneidermax2118@gmail.com>
2022-10-02 16:33:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 import sys
 import logging
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 
 @app.route("/")
