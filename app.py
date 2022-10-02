632f662076b88a80a98481a35b1a15e18d3dff09
zero <schneidermax2118@gmail.com>
2022-10-02 17:28:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def home():
-    return render_template("chicken.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
