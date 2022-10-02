0e105dba9393bbe6b22ade68cabae12665876417
zero <schneidermax2118@gmail.com>
2022-10-02 17:16:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, redirect, url_for, render_template 
 import sys
-import os
+
 import logging
 
-template_dir = os.path.abspath('/templates/')
-static_dir = os.path.abspath('/static/')
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
